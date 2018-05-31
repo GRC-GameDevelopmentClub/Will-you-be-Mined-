8244fdcb95ac9c389eae792981c7410cba1e96be
Cohav0310 <36090325+Cohav0310@users.noreply.github.com>
2018-05-31 12:41:31 -0700

    UI
    I added in a countdown timer, player health, and a death and time's up screen.

---
+++ Will you be Mined/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterface : MonoBehaviour {
+
+    public Text timerText;
+    public float time;
+
+    public GameObject timeUpScreen;
+    public GameObject timesUpText;
+    public float textFadeIn;
+
+	// Use this for initialization
+	void Start () {
+        timerText = GetComponent<Text>();
+        timeUpScreen.SetActive(false);
+        timesUpText.SetActive(false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timerText.text = "" + Mathf.Round(time);
+        time -= Time.deltaTime;
+
+        if(time <= 0)
+        {
+            timeUpScreen.SetActive(true);
+            textFadeIn -= Time.deltaTime;
+            if (textFadeIn <= 0)
+            {
+                timesUpText.SetActive(true);
+            }
+        }
+	}
+}
