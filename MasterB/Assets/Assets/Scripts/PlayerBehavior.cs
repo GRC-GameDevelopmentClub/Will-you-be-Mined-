530d285372495423c7cd98c055df2fadae4f8e05
BenH <blagetheyordle@gmail.com>
2018-05-31 17:06:36 -0700

    BenChanges
    added placeholder gem, when you rub into it you switch scenes.

---
+++ MasterB/Assets/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+
+    public float moveSpeed;
+
+
+    private Rigidbody2D rb;
+
+    public float playerHealth;
+
+    public GameObject attackObject;
+
+    public GameObject deathScreen;
+
+    public GameObject heartOne;
+    public GameObject heartTwo;
+    public GameObject heartThree;
+
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        deathScreen.SetActive(false);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(moveSpeed, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-moveSpeed, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            attackObject.SetActive(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            attackObject.SetActive(false);
+        }
+
+        if(playerHealth == 2)
+        {
+            heartOne.SetActive(false);
+        }
+
+        if (playerHealth == 1)
+        {
+            heartTwo.SetActive(false);
+        }
+
+
+        if (playerHealth <= 0)
+        {
+            heartThree.SetActive(false);
+            Debug.Log("You Died!");
+            deathScreen.SetActive(true);
+        }
+    }
+
+}
+
---
+++ MasterB/Assets/SceneChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChange : MonoBehaviour {
+
+    void Start()
+    {
+    }
+
+void OnCollisionEnter2D (Collision2D col)
+    {
+
+        Scene scene = SceneManager.GetActiveScene();
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        print("collision");
+
+        if (col.gameObject.name == "Gem")
+        {
+            if (currentSceneName == "Scene1")
+            {
+                SceneManager.LoadScene("Scene2");
+            }
+
+            if (currentSceneName == "Scene2")
+            {
+                SceneManager.LoadScene("Scene1");
+            }
+        }
+    }
+}
